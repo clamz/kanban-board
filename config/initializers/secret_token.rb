a5b99dd758e6419a167591db000b45cf48ea3b34
dguezou <dguezou@dguezou>
2013-09-05 17:54:11 +0200

    initialize project on git hub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kanbanboard::Application.config.secret_key_base = 'e0cf523b3fd319f149fbb855ec217f2afcc8415513717c2f083794ec1c8340d2c59514b4c84b09e30dd3de6c4fd1944488fba2fbc9d231a7a31836f00db37817'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kanbanboard::Application.config.session_store :cookie_store, key: '_kanbanboard_session'
